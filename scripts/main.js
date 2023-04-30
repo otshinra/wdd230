903c237a416e6f67bfb7b7bfff67ac49bbd41104
otshinra <104638109+otshinra@users.noreply.github.com>
2023-04-30 11:18:03 +0200

    another 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,12 @@
-const options = { weekday: "long", day: "numeric", month: "long", year: "numeric" };
-document.getElementById("currentdate").textContent = new Date().toLocaleDateString("en-US", options);
+// const options = { weekday: "long", day: "numeric", month: "long", year: "numeric" };
+// document.getElementById("currentdate").textContent = new Date().toLocaleDateString("en-US", options);
+
+const date = new Date();
+
+let year = date.getFullYear();
+
+let currentYear = `${year}`;
+
+document.querySelector("#year").textContent = currentYear;
+
+document.getElementById("currentdate").textContent = document.lastModified;
